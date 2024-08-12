046b1de9f5d41a69861a22e887bd73df2d387632
Victoria Rosa <vixkirosa@gmail.com>
2024-08-12 01:08:50 +0000

    Added email attributes to check if email has characterisitics of an email. Added an if conditional to check if number is the length of a number

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 class ExitProgram(Exception):
   pass
-
-#This is for getting th response from the user
+# to get the input fromt the user and exit at anytime
 def get_input(prompt):
-  #The variable for recieving th repsonse
   response = input(prompt + " (Press X to exit): ")
-  # If the user presses x we outta here
   if response.upper() == 'X':
       raise ExitProgram
   return response
+#Make sure the email is a valid email
+def email_attributes(prompt):
+  while True:
+      response = input(prompt + " (Email must have @, gmail.com, yahoo.com)")
+      email_necessities = ['@', 'gmail.com', 'yahoo.com', 'dot.net']
+      for char in email_necessities:
+          if char in response:
+              return response
+      else:
+          print("Invalid email. Must include @, gmail.com, yahoo.com or dot.net")
 
+# The start of the program
 def main():
   contact = {}
   contact_list = []
@@ -18,8 +26,13 @@ def main():
 
       while True:
           user_name = get_input("What is your name?")
-          user_phone_number = get_input("What is their number?")
-          user_email = get_input("What is their email?")
+          while True:
+              user_phone_number = get_input("What is their number?")
+              if len(user_phone_number) != 10:
+                  print("Invalid phone number! Must be 10 digits!")
+              else:
+                  break
+          user_email = email_attributes("What is their email?")
 
           contact[user_name] = user_phone_number, user_email
           contact_list.append(contact)
